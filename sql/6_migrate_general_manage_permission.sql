48b462e134b72f3a8bd7393aafed190650807943
Cliff Parnitzky <github@cliff-parnitzky.de>
2014-03-13 00:01:32 +0100

    Add sql file to set default for 'general_manage_permission'

---
+++ sql/6_migrate_general_manage_permission.sql
@@ -0,0 +1,10 @@
+-- **********************************************************
+-- *                                                        *
+-- * IMPORTANT NOTE                                         *
+-- *                                                        *
+-- * This file has to be imported manually                  *
+-- * The install tool will ignore it!                       *
+-- *                                                        *
+-- **********************************************************
+
+UPDATE tl_dms_categories SET general_manage_permission = "CUSTOM";
